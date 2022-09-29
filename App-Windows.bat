0a20dc5efc621d2045c164715f5a640c1e0bf6ea
Gsuitenew <54498519+Motdulieu@users.noreply.github.com>
2022-09-30 01:11:26 +0700

    Update App-Windows.bat

--- App-Windows.bat
+++ App-Windows.bat
@@ -2,10 +2,10 @@
 # Click phải Save as(Ctr + S) để tải file .bat (bỏ đuôi .txt)
 @echo off 
 
-start msedge -inprivate "https://www.fshare.vn/file/DFR9OCH3Q7GE" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/AirExplorer4.0.1.zip" "https://www.fosshub.com/MKVToolNix.html?dwl=mkvtoolnix-64-bit-68.0.0.7z" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/UltraISO.Premium.Edition.9.5.2.Build.2836(Motdulieu).zip" "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-611.exe" "https://files2.codecguide.com/K-Lite_Codec_Pack_1700_Full.exe" "https://mirror2.internetdownloadmanager.com/idman641build1.exe?v=lt&filename=idman641build1.exe" "https://mirror.downloadvn.com/videolan/vlc/3.0.17.4/win32/vlc-3.0.17.4-win32.exe" "https://downloads.rclone.org/v1.56.2/rclone-v1.56.2-windows-amd64.zip" "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" "https://t1.daumcdn.net/potplayer/PotPlayer/Version/Latest/PotPlayerSetup64.exe" "https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe" 
+start msedge -inprivate "https://www.fshare.vn/file/DFR9OCH3Q7GE" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/UniKeyNT.exe" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/AirExplorer4.0.1.zip" "https://www.fosshub.com/MKVToolNix.html?dwl=mkvtoolnix-64-bit-68.0.0.7z" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/UltraISO.Premium.Edition.9.5.2.Build.2836(Motdulieu).zip" "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-611.exe" "https://files2.codecguide.com/K-Lite_Codec_Pack_1700_Full.exe" "https://mirror2.internetdownloadmanager.com/idman641build2.exe?v=lt&filename=idman641build2.exe" "https://mirror.downloadvn.com/videolan/vlc/3.0.17.4/win32/vlc-3.0.17.4-win32.exe" "https://raw.githubusercontent.com/Motdulieu/App-Windows/main/rclone1592.exe" "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip" "https://t1.daumcdn.net/potplayer/PotPlayer/Version/Latest/PotPlayerSetup64.exe" "https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe" 
 timeout 8
 start shell:Downloads
-start C:\Users\%USERNAME%\Downloads\idman641build1.exe
+start C:\Users\%USERNAME%\Downloads\idman641build2.exe
 start C:\Users\%USERNAME%\Downloads\FshareSetup_5.3.0.174.exe
 start C:\Users\%USERNAME%\Downloads\winrar-x64-611.exe
 start C:\Users\%USERNAME%\Downloads\python-3.10.5-amd64.exe
